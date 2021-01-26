ded486aa4c20c5e713f40497351198981af7b590
Haixuan Xavier Tao <hai-xuan.tao@student.ecp.fr>
2021-01-26 20:59:54 +0100

    Create 4-polygon.js

---
+++ 4-polygon.js
@@ -0,0 +1,33 @@
+// TO MAKE THE MAP APPEAR YOU MUST
+// ADD YOUR ACCESS TOKEN FROM
+// https://account.mapbox.com
+mapboxgl.accessToken = 'TOKEN';
+var map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  center: [2.213749, 46.227638],
+  zoom: 4
+});
+
+map.on('load', function() {
+  map.addSource('maine', {
+    'type': 'geojson',
+    'data': ''
+  });
+  map.addLayer({
+    'id': 'maine',
+    'type': 'fill',
+    'source': 'maine',
+    'layout': {},
+    'paint': {
+      'fill-color': '#088',
+      'fill-opacity': 0.8
+    }
+  });
+});
+map.addControl(
+  new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+  })
+);
